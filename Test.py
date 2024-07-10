921d209a9d7ce2f96577d19a25571ff4a37e38d6
EXEyad <151939756+EXEyad@users.noreply.github.com>
2024-07-10 06:08:10 +0000

    Reload

--- Test.py
+++ Test.py
@@ -1,9 +1,11 @@
 from XPyDevTools.Functions import Functions
+import inspect
 
 @Functions
-def main():
+def main(name):
     print('Test')
+print('Test')
     print('Test')
-    print('Test')
+
 main.init(__file__)
 main.debug()
--- XPyDevTools/Functions.py
+++ XPyDevTools/Functions.py
@@ -1,21 +1,28 @@
+import inspect
 import os
 
 class Functions:
-    def __init__(self,func):
+    def __init__(self,func,*args, **kwargs):
         try:
             if self.func:
-                
+            
                 if self.filepath :
                     self.funcname = func.__name__
                     self.func = func
+                    self.args = inspect.getfullargspec(self.func).args
                 else:
                     self.funcname = func.__name__
+                    self.args = inspect.getfullargspec(self.func).args
                     self.func = func
             else:
+                self.funcname = func.__name__
                 self.func = func
+                self.args = inspect.getfullargspec(self.func).args
                 
         except AttributeError:
+            self.funcname = func.__name__
             self.func = func
+            self.args = inspect.getfullargspec(self.func).args
             pass
                 
     def init(self,FilePath):
@@ -45,21 +52,42 @@ class Functions:
                         found = True
                     else:
                         continue
-                print(ffl[ind:len(ffl)])
-        finally:
-            pass 
-# except:
-#            print('You must Init your function.')
+                d =1
+                x=1
+                fu = f'def {self.funcname}({self.args[0]}):\n'
+                fuend =False
+                for x in range(len(ffl[ind:len(ffl)])):
+                    if x == 1 :
+                        if ffl[ind+x] == fu:
+                            print("def without errors")
+                        else:
+                            print(ffl[ind+x])
+                            print('Error\n')
+                            print(fu)
+                        break
+                    elif x <= 2:
+                        if fuend == False and ffl[ind+x][0:5] == '    ':
+                            print('indentation good!')
+                            continue
+                        else:
+                            fuend =True
+                        if fuend == True and ffl[ind+x][0:5] == '    ':
+                            print(f"{len(ffl) - ind+x}{ffl[ind+x - 1]} ")
+                        else:
+                            print(f"Function {self.funcname} without errors for now.:)")                  
+        except AttributeError as AE:
+            print(AE)
+            print('DEBUGERROR:You must Init your function.')
         
         
-        try:
-            self.func()
-        except IndentationError:
-            print("There\'s an Indentation Error.")
-        except SyntaxError:
-            print('Rewrite Code.')
-        except ImportError:
-            print("There\'s an error while importing.")
+        #try:
+        #    self.func()
+        #except IndentationError:
+        #    print("There\'s an Indentation Error.")
+        #except SyntaxError:
+        #    print('Rewrite Code.')
+        #except ImportError:
+        #    print("There\'s an error while importing.")
 
         
             
